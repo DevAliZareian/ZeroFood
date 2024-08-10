50decfd87d9c7e0a75524839816ae0334104b88b
DevAliZareian <154817911+DevAliZareian@users.noreply.github.com>
2024-08-10 19:57:57 +0330

    feat: LoginForm Added to the reastaurant page

--- client/src/components/app/restaurant/Page.jsx
+++ client/src/components/app/restaurant/Page.jsx
@@ -4,15 +4,21 @@ import Form from "./interface/Form";
 import Information from "./interface/Information";
 import Menu from "./interface/Menu";
 import { useFetchDataOnLoad } from "../../../hooks/useFetchDataOnLoad";
+import LoginForm from "../../authentication/LoginForm";
+import { usePopup } from "../../../contexts/PopupContext";
 
 export default function Page() {
   let { id } = useParams();
+  const { popup } = usePopup();
   const { data, isPending, error } = useFetchDataOnLoad(`https://8ggbxf58-3000.inc1.devtunnels.ms/detail/rest?id=${id}`, id);
   return (
-    <Form data={data} isPending={isPending} error={error}>
-      <Information data={data} />
-      <Menu data={data} />
-      <Basket />
-    </Form>
+    <>
+      {popup.loginForm && <LoginForm />}
+      <Form data={data} isPending={isPending} error={error}>
+        <Information data={data} />
+        <Menu data={data} />
+        <Basket />
+      </Form>
+    </>
   );
 }
