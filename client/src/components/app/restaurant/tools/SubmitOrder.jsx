f0f1af6142b937ee88cd40e45257042023a882ec
DevAliZareian <154817911+DevAliZareian@users.noreply.github.com>
2024-08-13 20:53:42 +0330

    style: submitOrders loading

--- client/src/components/app/restaurant/tools/SubmitOrder.jsx
+++ client/src/components/app/restaurant/tools/SubmitOrder.jsx
@@ -1,18 +1,41 @@
 import React from "react";
+import Loading from "react-loading";
+
 import { useOrders } from "../../../../contexts/OrdersContext";
+import { usePopup } from "../../../../contexts/PopupContext";
+import { useUser } from "../../../../contexts/UserContext";
 
 export default function SubmitOrder({ orders }) {
-  const { addOrder } = useOrders();
+  const { setPopup } = usePopup();
+  const { addOrder, isPending } = useOrders();
+  const { user } = useUser();
   return (
-    <div className="sticky bottom-0 w-full h-[7.5rem] bg-white">
-      <button
-        onClick={() => addOrder(orders)}
-        type="submit"
-        className="mt-[1rem] mb-auto w-full min-w-[6.6875rem] p-2 h-12 inline-flex items-center justify-center text-center text-white bg-green-500 border-none cursor-pointer duration-[0.3s] overflow-hidden rounded-[0.375rem] text-[1.125rem] font-bold"
-        dir="rtl"
-      >
-        ورود به حساب کاربری
-      </button>
-    </div>
+    <>
+      {user ? (
+        <div className="sticky bottom-0 w-full h-[7.5rem] bg-white">
+          <button
+            onClick={() => addOrder(orders)}
+            type="submit"
+            className="mt-[1rem] mb-auto w-full min-w-[6.6875rem] p-2 h-12 inline-flex items-center justify-center text-center text-white bg-green-500 border-none cursor-pointer duration-[0.3s] overflow-hidden rounded-[0.375rem] text-[1.125rem] font-bold"
+            dir="rtl"
+          >
+            {isPending ? <Loading type={"bubbles"} /> : "ثبت سفارش"}
+          </button>
+        </div>
+      ) : (
+        <div className="sticky bottom-0 w-full h-[7.5rem] bg-white">
+          <button
+            onClick={() => {
+              setPopup((prevState) => ({ ...prevState, loginForm: true }));
+            }}
+            type="submit"
+            className="mt-[1rem] mb-auto w-full min-w-[6.6875rem] p-2 h-12 inline-flex items-center justify-center text-center text-white bg-green-500 border-none cursor-pointer duration-[0.3s] overflow-hidden rounded-[0.375rem] text-[1.125rem] font-bold"
+            dir="rtl"
+          >
+            ورود به حساب کاربری
+          </button>
+        </div>
+      )}
+    </>
   );
 }
