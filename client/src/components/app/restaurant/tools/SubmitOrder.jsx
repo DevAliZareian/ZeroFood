e09d9e74d598ed04025f5023bc8274f6df675bad
DevAliZareian <154817911+DevAliZareian@users.noreply.github.com>
2024-08-31 19:11:23 +0330

    fix: submitOrder event.preventDefault

--- client/src/components/app/restaurant/tools/SubmitOrder.jsx
+++ client/src/components/app/restaurant/tools/SubmitOrder.jsx
@@ -14,7 +14,10 @@ export default function SubmitOrder({ orders, description }) {
       {user ? (
         <div className="sticky bottom-0 w-full h-[7.5rem] bg-white">
           <button
-            onClick={() => submitOrders({ orders: orders, description: description.current.value, id: user.id })}
+            onClick={(event) => {
+              event.preventDefault();
+              submitOrders({ orders: orders, description: description.current.value, id: user.id });
+            }}
             type="submit"
             className="mt-[1rem] mb-auto w-full min-w-[6.6875rem] p-2 h-12 inline-flex items-center justify-center text-center text-white bg-green-500 border-none cursor-pointer duration-[0.3s] overflow-hidden rounded-[0.375rem] text-[1.125rem] font-bold"
             dir="rtl"
@@ -25,7 +28,8 @@ export default function SubmitOrder({ orders, description }) {
       ) : (
         <div className="sticky bottom-0 w-full h-[7.5rem] bg-white">
           <button
-            onClick={() => {
+            onClick={(event) => {
+              event.preventDefault();
               setPopup((prevState) => ({ ...prevState, loginForm: true }));
             }}
             type="submit"
